8da2837e68c68c9198a12662f1c330873d20afe4
alecrmeyer <alecrmeyer4@gmail.com>
2023-02-28 19:25:23 -0600

    added midi to array sample for testing

--- Sandbox/MidiToArray.py
+++ Sandbox/MidiToArray.py
@@ -1,10 +1,12 @@
 from mido import MidiFile
 
-def MidiToArray():
+def MidiToArray(input_midi):
     midi_array = []
-    mid = MidiFile(r'C:\Users\alecr\desktop\mutab\data\0\Stairway To Heaven.mid', clip=True)
+    mid = MidiFile(input_midi, clip=True)
 
     for msg in mid.tracks[1]:
         if msg.type == 'note_on':
             midi_array.append(msg.note)
     return midi_array
+
+print(MidiToArray(r"C:\Users\alecr\desktop\mutab\Sandbox\samples\sample.mid"))
